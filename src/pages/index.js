99f36ae12287ecc18cbaa3a4018bfdb961054e62
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-02-17 19:50:27 +0500

    netlify functions created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,28 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useEffect , useState} from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+
+  const [data, setData] = useState("")
+
+  useEffect( () => {
+        const fetchData = async () => {
+          const response = await fetch('.netlify/functions/hello')
+           const Data = await response.json()
+          setData(Data)
+        }
+        fetchData();
+  } , [])
+
+  return(
+    <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <p>{data.message}</p>
   </Layout>
-)
+  )
+  
+}
 
 export default IndexPage
